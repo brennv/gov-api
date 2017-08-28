48dea14d1cd7aa308e4388b90003ed8673352c48
Brennan Vincello <brennanv03@gmail.com>
2017-08-28 03:39:05 -0700

    clean up

--- app.py
+++ app.py
@@ -5,20 +5,20 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/gov/api/')
+@app.route('/api/')
 def all_the_data():
     return jsonify(data)
 
 
-@app.route('/gov/api/twitter')
+@app.route('/api/twitter')
 def twitter_handles():
     return jsonify(twitter_ids)
 
 
-@app.route('/gov/api/tweets')
+@app.route('/api/tweets')
 def tweets():
     return jsonify(tweets)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(threaded=True)
