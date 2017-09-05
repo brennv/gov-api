9a5654aeb5a655d4f476708ffe8ce32c47f2a024
Brennan Vincello <brennanv03@gmail.com>
2017-09-05 02:11:50 -0700

    fix path

--- gov/data.py
+++ gov/data.py
@@ -5,11 +5,13 @@ import os
 
 base_path = os.path.dirname(__file__)
 
-filepath = os.path.join(base_path, '..', 'congress-legislators/legislators-current.yaml')
+base_path = '/'.join(base_path.split('/')[:-1])
+
+filepath = os.path.join(base_path, 'congress-legislators/legislators-current.yaml')
 with open(filepath) as f:
     congress_names = yaml.load(f)
 
-filepath = os.path.join(base_path, '..', 'congress-legislators/legislators-social-media.yaml')
+filepath = os.path.join(base_path, 'congress-legislators/legislators-social-media.yaml')
 with open(filepath) as f:
     congress_social = yaml.load(f)
 
